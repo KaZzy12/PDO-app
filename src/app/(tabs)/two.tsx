e1f7c9335307d02de41e41703902706bca53338a
KaZzy12 <xthel0@gmail.com>
2024-07-16 08:04:04 +0200

    more proper profile page + clean

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,15 +1,14 @@
 import { StyleSheet, Button } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
 import { supabase } from '@/src/lib/supabase';
+import { useAuth } from '@/src/providers/AuthProvider';
 
 export default function TabTwoScreen() {
+  const { profile } = useAuth();
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>{profile?.full_name}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
       <Button onPress={() => supabase.auth.signOut()} title='Se déconnecter'/>
     </View>
   );
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -55,7 +55,6 @@ function RootLayoutNav() {
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
           </Stack>
         </QueryProvider>
       </AuthProvider>
